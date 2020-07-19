f0882b85c7e16cc95e2a83a0dcc8d5f48d71813f
Taras Anpilov <t.anpilov@gmail.com>
2020-07-19 17:05:18 +0300

    version wih inputing date 1-st shift

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const options = {
     day: 'numeric',
 }
 
+const firstShift = {
+    text: 'first shift',
+    style: 'first_shift'
+}
+const secondShift = {
+    text: 'second shift',
+    style: 'second_shift'
+}
+const nightShift = {
+    text: 'night shift',
+    style: 'night_shift'
+}
+const dayOff = {
+    text: 'day off',
+    style: 'day_off'
+}
+
 closeBtn.addEventListener('click', closeInput)
 
 btn.addEventListener('click', ()=>{
@@ -31,7 +48,8 @@ class UserDate {
     }
     showDate(elem) {
         let shownDay = document.createElement('div') 
-        shownDay.innerHTML = `<p>${this.date.toLocaleString("uk", options)}</p><p>${this.shift}</p>`
+        shownDay.innerHTML = `<p>${this.date.toLocaleString("uk", options)}</p><p>${this.shift.text}</p>`
+        shownDay.classList.add(this.shift.style)
         elem.append(shownDay)
     }
 }
@@ -76,36 +94,58 @@ function createAfter (container, data1, data2) {
     }
 }
 
+
 function compare(num) { 
     let result
     if (num==14 || num==13 || num==12 || num==8 || num==7 || num==6) {
-        result = 'day off'
+        result = dayOff
     } else  if (num==11 || num==10 || num==9) {
-        result = 'night shift'
+        result = nightShift
     } else  if (num==5 || num==4 || num==3) {
-        result = 'second shift'
+        result = secondShift
     } else if (num==2 || num===1 || num===0) {
-        result = 'first shift' 
+        result = firstShift 
+    } return result
+}
+
+function compareBack(num) { 
+    let result
+    if (num==1 || num==2 || num==3 || num==7 || num==8 || num==9) {
+        result = dayOff
+    } else  if (num==4 || num==5 || num==6) {
+        result = nightShift
+    } else  if (num==10 || num==11 || num==12) {
+        result = secondShift
+    } else if (num==13 || num===14 || num===0) {
+        result = firstShift 
     } return result
 }
 
 function getShift(date, day) {
     date = +date
     day = +day
-    let dayType
+    let someShift    
     let x, y
     if (day>=date){
         x = (day-date)/86400000
-    } else {
-        alert('look forward!')
-    }
-    if (x>14) {
-        y = x%15
-        dayType = compare(y)
-    } else if (x) {    
-        dayType = compare(x)        
-    }
-    return dayType    
+        if (x>14) {
+            y = x%15
+            someShift = compare(y)
+        } else if (x>0) {    
+            someShift = compare(x)
+        } else if (x===0) {
+            someShift = firstShift              
+        }
+    } else if (day<date){
+        x = (date-day)/86400000
+        if (x>15) {
+            y = x%15
+            someShift = compareBack(y)
+        } else if (x) {    
+            someShift = compareBack(x)        
+        }        
+    }    
+    return someShift    
 }
 
 function cleanBox(box) {
